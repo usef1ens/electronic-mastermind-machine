b5aa2383fedba53b3a0102be6377a8a5a9cb8a6f
Youssef El Bassumi <youssef.bassumi@sabanciuniv.edu>
2025-12-30 22:58:28 +0300

    1st module

--- code_maker.v
+++ code_maker.v
@@ -1,9 +1,9 @@
-module code_maker(input clk, reset, codeMaker, player_A, player_B
-input [11:0] R1, SW[2:0],
-output reg active_p, take_code);
+module code_maker(input clk, reset, codeMaker, player_A, player_B, enterA, enterB,
+input SW[2:0],
+output reg active_p, take_code, started
+output reg [11:0] toR1);
 
 reg [2:0] ct1; // a counter that counts to 4 (we have numbers 0 to 7)
-reg [11:0] toR1; // the register of which value will be returned
 reg [1:0] state;
 parameter [1:0] IDLE = 2'b00; // define the state encodings
 parameter [1:0] PA = 2'b01;
@@ -13,6 +13,7 @@ always @ (posedge clk or posedge reset) // sequential block: state assignment
 begin
     if(reset == 1'b0)
     begin
+        state <= IDLE; // go to the initial state
         toR1 <= 12'd0; // nullify register
         ct1 <= 3'd0; // nullify internal counter
     end
@@ -21,6 +22,7 @@ begin
         case(state) // cover all states
         IDLE: // at the IDLE state
         begin
+            ct1 <= 3'd0; // nullify counter
             if(codeMaker) // if codemaker is 1
             begin
                 if(player_A) state <= PA; // the next state is Player A in case we chose player A
@@ -30,17 +32,26 @@ begin
         end
         PA: // at player A's state
         begin
-            toR1 <= {toR1[8:0], SW}; // push the 3bit input by left-shifting
-            ct1 <= ct1 + 1'b1; // increment the counter
-            if(ct1 == 3'd4) state <= IDLE; // if we have reached the limit then go back to IDLE
-            else state <= PA; // otherwise remain in the same state
+            if(enterA) // only if the codemaker enters these digits
+            begin
+                toR1 <= {toR1[8:0], SW}; // push the 3bit input by left-shifting
+                ct1 <= ct1 + 1'b1; // increment the counter 
+                if(ct1 == 3'd3) state <= IDLE; // same logic as in A's state 
+                // NOTE: (justify why are you checking the counter being at 3 and not 4)
+                else state <= PA; 
+            end
+            else state <= PA; // if enter was not pressed, check it at the next clk pulse
         end
         PB: // at player B's state
         begin
-            toR1 <= {toR1[8:0], SW}; // push the 3bit input by left-shifting
-            ct1 <= ct1 + 1'b1; // increment the counter
-            if(ct1 == 3'd4) state <= IDLE; // same logic as in A's state
-            else state <= PB;
+            if(enterB) // only if the codemaker enters these digits
+            begin
+                toR1 <= {toR1[8:0], SW}; // push the 3bit input by left-shifting
+                ct1 <= ct1 + 1'b1; // increment the counter 
+                if(ct1 == 3'd3) state <= IDLE; // same logic as in A's state
+                else state <= PB; 
+            end
+            else state <= PB; // if enter was not pressed, check it at the next clk pulse
         end
         default: state <= IDLE;
         // should we assigna a default value for ct1? 
@@ -50,31 +61,48 @@ begin
     end
 end
 
-always @ (*) // combinational block, for computing mealy outputs
+always @ (*) 
 begin
     case(state)
-    PA:
-    begin
-        if(ct1 == 3'd4)
+        PA: 
         begin
-            started = 1'b1;
-            active_p = 1'b0;
-            take_code = 1'b0;
+            if (ct1 == 3'd4) 
+            begin
+                started   = 1'b1;
+                active_p  = 1'b0;
+                take_code = 1'b0;
+            end
+            else 
+            begin
+                started   = 1'b0;
+                active_p  = 1'b0;
+                take_code = 1'b0;
+            end
         end
-        default: started = 1'b0, active_p = 1'b0, take_code = 1'b0;
-    end
-    PB:
-    begin
-        if(ct1 == 3'd4)
-        begin
-            started = 1'b1;
-            active_p = 1'b1;
-            take_code = 1'b0;
-        end
-        default: started = 1'b0, active_p = 1'b0, take_code = 1'b0;
-    end
-    endcase
 
+        PB: 
+        begin
+            if (ct1 == 3'd4) 
+            begin
+                started   = 1'b1;
+                active_p  = 1'b1;
+                take_code = 1'b0;
+            end
+            else 
+            begin
+                started   = 1'b0;
+                active_p  = 1'b1;
+                take_code = 1'b0;
+            end
+        end
+
+        default: 
+        begin // Covers IDLE state
+            started   = 1'b0;
+            active_p  = 1'b0;
+            take_code = 1'b0;
+        end
+    endcase
 end
 
 endmodule
