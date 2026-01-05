2dd91fdefb014ee007a585f3b1d9f4a11bef520d
Youssef El Bassumi <youssef.bassumi@sabanciuniv.edu>
2026-01-05 22:24:48 +0300

    added new files for FPGA interfacing

---
+++ clk_divider.v
@@ -0,0 +1,28 @@
+// DO NOT CHANGE THIS MODULE
+
+module clk_divider (
+  input      clk_in     ,
+  output reg divided_clk
+);
+
+  parameter  toggle_value = 270000;
+  reg [24:0] cnt                 ;
+
+  initial begin
+    cnt = 0;
+    divided_clk = 0;
+  end
+
+  always@(posedge clk_in)
+    begin
+      if (cnt==toggle_value) begin
+        cnt         <= 0;
+        divided_clk <= ~divided_clk;
+      end
+      else begin
+        cnt         <= cnt +1;
+        divided_clk <= divided_clk;
+      end
+    end
+
+endmodule
---
+++ top_module.v
@@ -0,0 +1,17 @@
+module top_module(
+    input clk,
+    input rst,
+    
+    input enterA,
+    input enterB,
+    input [2:0] letterIn,			 
+    
+    output [7:0] led,
+    output a_out,b_out,c_out,d_out,e_out,f_out,g_out,p_out,
+    output [3:0] an
+);
+    
+   
+	
+endmodule
+
